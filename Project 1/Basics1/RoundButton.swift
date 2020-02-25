094c9fdbf6046de58dcfecb17205c8931c3466a3
codegenius1017 <joem10067@gmail.com>
2020-02-25 16:54:37 +0100

    Round button shapes

--- Project 1/Basics1/RoundButton.swift
+++ Project 1/Basics1/RoundButton.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class RoundButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
     }
-    */
+    
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
 
 }
