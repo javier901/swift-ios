0ba6ab98d99ad237f494418117252f5d2482bc5c
codegenius1017 <joem10067@gmail.com>
2020-02-25 16:54:24 +0100

    Round button shapes

---
+++ Project 1/Basics1/RoundButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundButton.swift
+//  Basics1
+//
+//  Created by Debaleena on 25/02/2020.
+//  Copyright © 2020 LIRIS.sampleDemos. All rights reserved.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
